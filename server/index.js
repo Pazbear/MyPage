2a407767ba03a75f7b7b644c4bf8f666800c1ede
pazbear <mkcho1997@daum.net>
2020-04-17 18:41:46 +0900

    app making...

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const app = express();
+const path = ("path");
+const cors = ('cors');
 
-const
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI,{
+    useNewUrlParser : true, useUnifiedTopology : true,
+    useCreateIndex: true, useFindAndModify:false
+})
+.then(()=> console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.use('/api/users', require(''))
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+    console.log(`Server Running at ${port}`)
+})
